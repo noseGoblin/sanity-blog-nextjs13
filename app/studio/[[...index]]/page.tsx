be4e717165ebe3f2c31b17177e87ee35646015c0
Jacob Mompean <jaymompean@gmail.com>
2023-08-27 12:44:18 -0500

    setup studio route: `/studio`

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,8 +1,19 @@
-import { Studio } from './Studio';
+'use client';
 
-// Set the right `viewport`, `robots` and `referer` meta tags
-export { metadata } from 'next-sanity/studio/metadata';
+import Head from 'next/head';
+import { NextStudio } from 'next-sanity/studio';
+import { metadata } from 'next-sanity/studio/metadata';
+import config from '@/sanity.config';
 
 export default function StudioPage() {
-  return <Studio />;
+  return (
+    <>
+      <Head>
+        {Object.entries(metadata).map(([key, value]) => (
+          <meta key={key} name={key} content={value} />
+        ))}
+      </Head>
+      <NextStudio config={config} />
+    </>
+  );
 }
