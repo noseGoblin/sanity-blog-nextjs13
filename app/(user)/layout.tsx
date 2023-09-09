35040c15bbe69ef02c28db5c930fe0b1ea2727a0
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 14:51:06 -0500

    followed sanity preview app router guid here: https://github.com/sanity-io/next-sanity/blob/main/PREVIEW-app-router.md; needs debug; may need to apply to different / all layout & page routes

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -4,14 +4,21 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Banner from '@/components/Banner';
 
+import dynamic from 'next/dynamic';
+import { draftMode } from 'next/headers';
+import { token } from '@/sanity/lib/fetch';
+
 const inter = Inter({ subsets: ['latin'] });
+const PreviewProvider = dynamic(
+  () => import('../../components/PreviewProvider')
+);
 
 export const metadata: Metadata = {
   title: 'Jay&#39;s Personal Website',
   description: 'Powered by Sanity v3 and Next.js 13',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
@@ -21,7 +28,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <Banner />
-        {children}
+        {draftMode().isEnabled ? (
+          <PreviewProvider token={token}>{children}</PreviewProvider>
+        ) : (
+          children
+        )}
       </body>
     </html>
   );
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,25 @@
-import Image from 'next/image';
+import { draftMode } from 'next/headers';
+import { LiveQuery } from 'next-sanity/preview/live-query';
+import DocumentsCount, { query } from '@/components/DocumentsCount';
+import PreviewDocumentsCount from '@/components/PreviewDocumentsCount';
+import { sanityFetch } from '@/sanity/lib/fetch';
 
-export default function Home() {
+export default async function Home() {
+  const data = await sanityFetch<number>({ query, tags: ['post'] });
+
   return (
-    <div>
-      {/* <h1 className='text-4xl'>Welcome to the Blog, bitches</h1> */}
-    </div>
+    <>
+      <LiveQuery
+        enabled={draftMode().isEnabled}
+        query={query}
+        initialData={data}
+        as={PreviewDocumentsCount}
+      >
+        <DocumentsCount data={data} />
+      </LiveQuery>
+      <div>
+        {/* <h1 className='text-4xl'>Welcome to the Blog, bitches</h1> */}
+      </div>
+    </>
   );
 }
