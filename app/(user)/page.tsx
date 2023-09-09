7b725b785ccd7c50da3a9f5141c3c9aa86146672
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 15:44:25 -0500

    Create BlogList boiler component and typings

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image';
+import BlogList from '@/components/BlogList';
+import { client } from '@/sanity/lib/client';
+import { groq } from 'next-sanity';
+// import { SanityClient } from 'sanity';
 
-export default function Home() {
-  return (
-    <div>
-      {/* <h1 className='text-4xl'>Welcome to the Blog, bitches</h1> */}
-    </div>
-  );
+const query = groq`*[_type == 'post'] {
+  ...,
+  author->,
+  categories[]->,
+} | order(_createdAt desc)`;
+
+export default async function Home() {
+  const posts = await client.fetch(query);
+
+  return <BlogList posts={posts} />;
 }
